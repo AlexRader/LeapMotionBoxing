6034bc5b3b8c898e14a31f13002c82e8354a9469
Unknown <arader@comcast.net>
2019-04-08 19:16:45 -0400

    k i can now choose basic options

--- Attempt2/ugh/Assets/Scripts/MyHandTest.cs
+++ Attempt2/ugh/Assets/Scripts/MyHandTest.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 namespace Leap.Unity.Interaction
 {
+    
     public class MyHandTest : MonoBehaviour
     {
         public Hand handRef;
-        public GameObject myHand;
+        public GameObject myHand, handActive;
         public float distance;
         public float timer = 1.0f;
+
+        public bool firstList;
+
+        public GameObject[] checkers;
         // Use this for initialization
         void Start()
         {
+            firstList = false;
             distance = 0;
             handRef = myHand.GetComponent<InteractionHand>()._hand;
             //StartCoroutine(testing());
@@ -22,8 +28,7 @@ namespace Leap.Unity.Interaction
         void Update()
         {
             Debug.Log(handRef);
-            Debug.Log(handRef.Fingers[1].Type);
-            if (handRef.Fingers[1].Type != 0 && handRef.IsLeft)
+            if (handActive.activeSelf)//handRef.Fingers[1].Type != 0 && handRef.IsLeft)
             {
                 distance = Vector3.Distance(handRef.Fingers[1].bones[3].Center.ToVector3(),
                                 handRef.Fingers[1].bones[0].Center.ToVector3());
@@ -37,18 +42,71 @@ namespace Leap.Unity.Interaction
                     timer = 1;
                 }
             }
-            Debug.Log(myHand.activeSelf);
+            Debug.Log(handActive.activeSelf);
+
+            if (firstList)
+            {
+
+            }
         }
 
         public IEnumerator testing()
         {
             yield return new WaitForSeconds(1.0f);
-            if (myHand != null)
-            {
-                if (myHand.GetComponent<InteractionHand>()._hand.IsLeft)
-                    handRef = myHand.GetComponent<InteractionHand>()._hand;
-            }
+            //if (myHand != null)
+            //{
+            if (handActive.activeSelf && handActive.GetComponent<CapsuleHand>().Handedness == 0)
+            //if (myHand.GetComponent<InteractionHand>()._hand.IsLeft)
+                handRef = myHand.GetComponent<InteractionHand>()._hand;
+            //}
             //myHand = GameObject.Find("Interaction Hand (Left)");
+        }
+
+        public void TrackFirstList()
+        {
+            Debug.Log("systemStuff");
+            TurnOff();
+        }
+        public void TrackSecondList()
+        {
+            Debug.Log("abilitiesMovement");
+            TurnOff();
+        }
+        public void TrackThirdList()
+        {
+            Debug.Log("abilitiesAttack");
+            TurnOff();
+        }
+
+        void TurnOff()
+        {
+            ExtendedFingerDetector[] checks;
+            for (int i = 0; i < checkers.Length; ++i)
+            {
+                checks = checkers[i].GetComponents<ExtendedFingerDetector>();
+                Debug.Log(checks.Length);
+                foreach (ExtendedFingerDetector x in checks)
+                {
+                    x.enabled = false;
+                    Debug.Log(x.IsActive);
+                }
+            }
+            firstList = !firstList;
+        }
+        void TurnOn()
+        {
+            ExtendedFingerDetector[] checks;
+            for (int i = 0; i < checkers.Length; ++i)
+            {
+                checks = checkers[i].GetComponents<ExtendedFingerDetector>();
+                Debug.Log(checks.Length);
+                foreach (ExtendedFingerDetector x in checks)
+                {
+                    x.enabled = true;
+                    Debug.Log(x.IsActive);
+                }
+            }
+            firstList = !firstList;
         }
 
     }
